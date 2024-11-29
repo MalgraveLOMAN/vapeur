cc8f6061cf6e7fa274c018744fa56265b31cc4ca
Malgrave <rodeurwill@gmail.com>
2024-11-29 14:25:23 +0100

    seeding fix

---
+++ README.md
@@ -0,0 +1,4 @@
+How to start Vapeur ? 
+
+Run at filepath\vapeur>
+This command : 
---
+++ prisma/seed.js
@@ -0,0 +1,26 @@
+const {PrismaClient} = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.typeEnum.createMany({
+        data: [
+            { type: 'NOTSET' },
+            { type: 'Action' },
+            { type: 'Aventure' },
+            { type: 'RPG' },
+            { type: 'Simulation' },
+            { type: 'Sport' },
+            { type: 'MMORPG' },
+        ],
+    });
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
