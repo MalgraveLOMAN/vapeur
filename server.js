8b2a7db4cdbc73bd615cd3ec76ec8442ec6c5818
Malgrave <rodeurwill@gmail.com>
2024-12-03 00:24:43 +0100

    Editor delete (ajouter button) - BDD edit (editor delete)

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { PrismaClient } = require("@prisma/client");
 const bodyParser = require("body-parser");
 const hbs = require("hbs");
 const path = require("path");
+const { deserialize } = require("v8");
 
 // App creation
 const prisma = new PrismaClient();
@@ -26,7 +27,11 @@ app.use(async (req, res, next) => {
     try {
         if (!TypesEditorObject.types || !TypesEditorObject.editors) {
             TypesEditorObject.types = await prisma.Type.findMany();
-            TypesEditorObject.editors = await prisma.Editor.findMany();
+            TypesEditorObject.editors = await prisma.Editor.findMany({
+                where: {
+                    active: true,
+                }
+            });
         }
         res.locals.types = TypesEditorObject.types;
         res.locals.editors = TypesEditorObject.editors;
@@ -57,13 +62,13 @@ async function CheckInsert() {
         }
         await prisma.Type.createMany({
             data: [
-                { type: 'NOTSET', description: 'Genre non renseigné' },
                 { type: 'Action', description: 'Jeux rapides axés sur les combats et la coordination.' },
                 { type: 'Aventure', description: 'Exploration et résolution d\'énigmes avec une forte narration.' },
-                { type: 'RPG', description: 'Jouer un personnage qui évolue dans une histoire et un monde.' },
-                { type: 'Simulation', description: 'Reproduction réaliste d’activités ou de systèmes réels.' },
+                { type: 'RPG', description: 'Inspiré des jeux de table : un personnage qui évolue dans une histoire et un monde.' },
+                { type: 'Simulation', description: 'Reproduction d\'activité ou d\'action dans divers environnements.' },
                 { type: 'Sport', description: 'Simulation de compétitions sportives.' },
-                { type: 'MMORPG', description: 'Jeu de rôle en ligne avec de nombreux joueurs dans un monde persistant.' },
+                { type: 'MMORPG', description: 'Jeu de rôle en ligne massivement multijoueur' },
+                { type: 'MOBA', description: 'Jeu de de bataille dans une arène en ligne multijoueur' },
             ],
         });
     } catch (error) {
@@ -83,21 +88,34 @@ async function DataTests() {
     try {
         await prisma.Editor.createMany({
             data: [
-                { name: 'Nintendo' },
-                { name: 'RiotGames' },
-                { name: 'NaughtyDog' },
+                {
+                    name: 'Nintendo',
+                    description: "Nintendo est une entreprise multinationale japonaise par Yamauchi Fusajirō8 à Kyoto. Elle est spécialisée dans la fabrication de consoles de jeu vidéo depuis 1977 avec la sortie de la Color TV-Game, ainsi que dans la conception de jeux vidéo, dont les séries Mario et The Legend of Zelda."
+                },
+                {
+                    name: 'RiotGames',
+                    description: "Riot Games est une entreprise américaine d'édition et de développement de jeux vidéo fondée en 2006 et située à Los Angeles, en Californie. Elle organise aussi plusieurs tournois de sport électronique."
+                },
+                {
+                    name: 'NaughtyDog',
+                    description: "Naughty Dog est une société américaine de développement de jeu vidéo domiciliée à Santa Monica, fondée en 1984 par Andy Gavin et Jason Rubin sous le nom de JAM Software, avant d'être rebaptisé Naughty Dog en 1989."
+                },
+                {
+                    name: 'FromSoftware',
+                    description: "FromSoftware, Inc. (株式会社フロム・ソフトウェア, Kabushikigaisha Furomu Sofutowea?) est une entreprise japonaise de développement de jeux vidéo, fondée en 1986 et située à Tokyo. Elle est notamment connue pour avoir créé les séries Dark Souls et Armored Core, ainsi que les jeux Bloodborne (2015), Sekiro: Shadows Die Twice (2019) et Elden Ring (2022).",
+                }
             ],
         });
 
         const types = await prisma.Type.findMany();
 
-        const typeNotSet = await types.find(type => type.type === 'NOTSET');
         const typeAction = await types.find(type => type.type === 'Action');
         const typeAventure = await types.find(type => type.type === 'Aventure');
         const typeRPG = await types.find(type => type.type === 'RPG');
         const typeSimulation = await types.find(type => type.type === 'Simulation');
         const typeSport = await types.find(type => type.type === 'Sport');
         const typeMMORPG = await types.find(type => type.type === 'MMORPG');
+        const typeMOBA = await types.find(type => type.type === 'MOBA');
 
         if (games.length > 0) {
             return;
@@ -108,43 +126,43 @@ async function DataTests() {
                     title: 'The Legend of Zelda: Breath of the Wild',
                     description: 'Un jeu d\'aventure où le joueur explore un monde ouvert.',
                     releaseDate: new Date('2017-03-03'),
-                    typeId: typeAv - enture.id,
-                    editorId: (await prisma.Editor.findUnique({ where: { name: 'Nintendo' } })).id,
+                    typeId: 2,
+                    editorId: 1
                 },
                 {
                     title: 'Super Mario Odyssey',
                     description: 'Un jeu de plateforme en 3D où Mario doit sauver Princess Peach.',
                     releaseDate: new Date('2017-10-27'),
-                    typeId: typeAction.id,
-                    editorId: (await prisma.Editor.findUnique({ where: { name: 'Nintendo' } })).id,
+                    typeId: 1,
+                    editorId: 1
                 },
                 {
                     title: 'League of Legends',
                     description: 'Un jeu de type MOBA où deux équipes s’affrontent pour détruire la base ennemie.',
                     releaseDate: new Date('2009-10-27'),
-                    typeId: typeMMORPG.id,
-                    editorId: (await prisma.Editor.findUnique({ where: { name: 'RiotGames' } })).id,
+                    typeId: 7,
+                    editorId: 2
                 },
                 {
                     title: 'Valorant',
                     description: 'Un jeu de tir tactique où chaque personnage possède des capacités uniques.',
                     releaseDate: new Date('2020-06-02'),
-                    typeId: typeAction.id,
-                    editorId: (await prisma.Editor.findUnique({ where: { name: 'RiotGames' } })).id,
+                    typeId: 1,
+                    editorId: 2
                 },
                 {
                     title: 'The Last of Us Part II',
                     description: 'Un jeu d\'action-aventure dans un monde post-apocalyptique.',
                     releaseDate: new Date('2020-06-19'),
-                    typeId: typeRPG.id,
-                    editorId: (await prisma.Editor.findUnique({ where: { name: 'NaughtyDog' } })).id,
+                    typeId: 3,
+                    editorId: 3
                 },
                 {
                     title: 'Uncharted 4: A Thief\'s End',
                     description: 'Un jeu d\'aventure avec des énigmes et des combats intenses.',
                     releaseDate: new Date('2016-05-10'),
-                    typeId: typeAction.id,
-                    editorId: (await prisma.Editor.findUnique({ where: { name: 'NaughtyDog' } })).id,
+                    typeId: 1,
+                    editorId: 3
                 },
             ],
         });
@@ -168,16 +186,46 @@ DataTests();
 
 // Create Data Section
 
-app.post("/gameCreate", function (req, res) {
-    console.log(req.body);
-    const referer = req.get("referer");
-    res.redirect(referer);
+app.post("/gameCreate", async function (req, res) {
+    try {
+        const {
+            'game-title': title,
+            'game-description': description,
+            'game-editor': editorId,
+            'game-type': typeId,
+            'game-release-date': releaseDate
+        } = req.body;
+        await prisma.game.create({
+            data: {
+                title,
+                description,
+                releaseDate: new Date(releaseDate),
+                typeId: parseInt(typeId),
+                editorId: parseInt(editorId),
+            },
+        });
+        res.redirect(req.get("referer"));
+    } catch (error) {
+        console.error("An error has occured : ", error);
+        res.status(500).send("An error has occured.");
+    }
 });
 
-app.post("/editorCreate", function (req, res) {
-    console.log(req.body);
-    const referer = req.get("referer");
-    res.redirect(referer);
+app.post("/editorCreate", async function (req, res) {
+    try {
+        const {
+            'game-editor-name': name
+        } = req.body;
+        await prisma.editor.create({
+            data: {
+                name,
+            },
+        });
+        res.redirect(req.get("referer"));
+    } catch (error) {
+        console.error("An error has occured : ", error);
+        res.status(500).send("An error has occured.");
+    }
 });
 
 // Read Data Section
@@ -265,10 +313,10 @@ app.get("/games/:id", async (req, res) => {
 
 // Afficher les listEditors.hbs (Liste de tous les éditeurs de jeux)
 app.get("/editors", async (req, res) => {
-    const editors = await prisma.Editor.findMany();
+    const a_editors = await prisma.Editor.findMany();
     res.render("editors/listEditors", {
         title: "Vapeur - Editors",
-        editors,
+        a_editors,
     });
 });
 
@@ -285,11 +333,16 @@ app.get("/games/editor/:id", async (req, res) => {
             },
         },
     });
-    formatGameDates(editor.games);
-    res.render("editors/gamesByEditor", {
-        title: `Vapeur - ${editor.name}`,
-        editor,
-    });
+    if (editor.active) {
+        formatGameDates(editor.games);
+        res.render("editors/gamesByEditor", {
+            title: `Vapeur - ${editor.name}`,
+            editor,
+        });
+    }
+    else {
+        res.redirect("/404");
+    }
 });
 
 // Update Data Section
