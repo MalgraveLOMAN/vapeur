9bd73fed1422f18bb5e3e7a1d1b978b9db262331
Mathieu <157475527+Funeralon@users.noreply.github.com>
2024-12-15 22:59:00 +0100

    Bouton Redirection

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    document.querySelectorAll(".form-popup").forEach(f => {
+        f.style.display = "none";
+    });
     const formActions = {
         "game-form": {
             "remove-front": "/removeFront",
@@ -94,7 +96,3 @@ function Click(event) {
         openFormId = null;
     }
 }
-
-
-
-
